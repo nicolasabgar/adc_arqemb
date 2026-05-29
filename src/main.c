f68f72e394b72b61bc73e4a6872680a677425028
Aluno <nicolas.abgar@usp.br>
2026-05-29 10:08:55 -0300

    add: finished adc blue led implementation

--- src/main.c
+++ src/main.c
@@ -3,6 +3,11 @@
 
 int main(void)
 {
+
+    //------------------------------------
+    // Configuracoes do ADC
+    //------------------------------------
+
     // Ativar clock da porta B
     SIM->SCGC5 |= (1 << 10);
 
@@ -21,13 +26,36 @@ int main(void)
     // Definir modo em 12 bits
     ADC0->CFG1 = (ADC0->CFG1 & ~0b11) | 0b01;
 
-    // Selecionar canal de entrada
+    //------------------------------------
+    // Configuracoes do GPIO
+    //------------------------------------
+
+    PORTD->PCR[1] |= (1 << 8); // Configurar MUX do GPIO
+
+    FGPIOD->PDDR |= (1 << 1); // Definir direcao de saida
+
+    
 
     // Loop infinito
     for (;;)
     {
 
+        // Selecionar canal de entrada (pino utilizado PTB0 -> ADC0_SE8 -> Canal: AD8)
+        ADC0->SC1[0] = (ADC0->SC1[0] & ~0b11111) | 0b01000;
 
+        // Esperar ate fim de conversao
+        while ( !(ADC0->SC1[0] & 0b10000000) ) {}
+
+        // Salvar resultado em 12 bits
+        uint16_t result = ADC0->R[0] & 0x0FFF;
+
+        if (result > 512) {
+            GPIOD->PCOR |= (1 << 1);
+        } else {
+            GPIOD->PSOR |= (1 << 1);
+        }
+
+        printk("Valor: %d \n", result);
 
     }
 
